0850dc0434940302e847ef808b82a005447b39b7
san2488 <sujay.narsale@gmail.com>
2013-09-30 17:21:30 -0400

    Clean up testing code

--- Project2_Del1.java
+++ Project2_Del1.java
@@ -9,8 +9,7 @@ class Project2_Del1 {
     public static void main(String[] args) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        OptimizedDeviceConfigParser odcf = new OptimizedDeviceConfigParser();
+        
         DeviceConfigParser dcf = new DeviceConfigParser();
         
         
@@ -23,56 +22,31 @@ class Project2_Del1 {
                 String[] inputs = command.split(" ");
                 if (inputs[0].equals("Configuration")) {
                     String filename = inputs[1];
-                    odcf.loadXML(filename);
                     dcf.loadXML(filename);
-                } else if (odcf.hasLoadedXML() && inputs.length == 1 && inputs[0].equals("DPDevice")) {
-                    int deviceCount = 0;
-                    
-                    startTime = System.nanoTime();
-                    deviceCount = odcf.getDPDeviceCount();
-                    endTime = System.nanoTime();
-                    System.out.println("Optimized Time: " + (endTime - startTime));
+                } else if (dcf.hasLoadedXML() && inputs.length == 1 && inputs[0].equals("DPDevice")) {
+                    int deviceCount = dcf.getDPDeviceCount();
                     System.out.println(deviceCount);
-                    
-                    startTime = System.nanoTime();
-                    deviceCount = dcf.getDPDeviceCount();
-                    endTime = System.nanoTime();
-                    System.out.println("Non-optimized Time: " + (endTime - startTime));
-                    System.out.println(deviceCount);
-                } else if(odcf.hasLoadedXML() && inputs.length == 3 && inputs[0].equals("DPDevice") 
+                } else if(dcf.hasLoadedXML() && inputs.length == 3 && inputs[0].equals("DPDevice") 
                         && inputs[2].equals("DPDomain")) {
-                    int domainCount = 0;
-                    
-                    startTime = System.nanoTime();
-                    domainCount = odcf.getDPDeviceDomains(inputs[1]);
-                    endTime = System.nanoTime();
-                    System.out.println("Optimized Time: " + (endTime - startTime));
+                    int domainCount = dcf.getDPDeviceDomains(inputs[1]);
                     System.out.println(domainCount);
-                    
-                    startTime = System.nanoTime();
-                    domainCount = dcf.getDPDeviceDomains(inputs[1]);
-                    endTime = System.nanoTime();
-                    System.out.println("Non-optimized Time: " + (endTime - startTime));
-                    System.out.println(domainCount);
-                    
-                } else if(odcf.hasLoadedXML() && inputs.length == 5 && inputs[0].equals("DPDevice") 
+                } else if(dcf.hasLoadedXML() && inputs.length == 5 && inputs[0].equals("DPDevice") 
                         && inputs[2].equals("DPDomain")
                         && inputs[4].equals("DeploymentPolicy")) {
-                    int deploymentPolicyCount = odcf.getDeployPolicyCount(inputs[1], inputs[3]);
+                    int deploymentPolicyCount = dcf.getDeployPolicyCount(inputs[1], inputs[3]);
                     System.out.println(deploymentPolicyCount);
-                } else if(odcf.hasLoadedXML() && inputs.length == 3 && inputs[0].equals("DeploymentPolicy") 
+                } else if(dcf.hasLoadedXML() && inputs.length == 3 && inputs[0].equals("DeploymentPolicy") 
                         && inputs[2].equals("Serviceendpoint")) {
-                    int servicePolicyCount = odcf.getServicePolicyCount(inputs[1]);
+                    int servicePolicyCount = dcf.getServicePolicyCount(inputs[1]);
                     System.out.println(servicePolicyCount);
-                } else if(odcf.hasLoadedXML() && inputs.length == 4 && inputs[0].equals("DeploymentPolicy") 
+                } else if(dcf.hasLoadedXML() && inputs.length == 4 && inputs[0].equals("DeploymentPolicy") 
                         && inputs[2].equals("Serviceendpoint")) {
-                    odcf.printServiceEndPointAttributes(inputs[1], inputs[3]);
+                    dcf.printServiceEndPointAttributes(inputs[1], inputs[3]);
                 } else {
                     System.out.println("Invalid command. Did you load the configuration file?");
                 }
                 
-                command = br.readLine();  
-                odcf.reset(); 
+                command = br.readLine();
                 dcf.reset();
             }
         } catch (IOException ioe) {
